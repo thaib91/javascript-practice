51ff261c4a79e4081125f60f3e45e0cccd0c28a6
Thai Bui <tbui64@gmail.com>
2019-03-12 19:01:34 -0400

    print vowels then consonants

--- index.js
+++ index.js
@@ -267,3 +267,21 @@ var myArray = [3, 0, 2, 5, -1, 4, 1 ];
 console.log("Original array: " + myArray);
 var sortedArray = quick_Sort(myArray);
 console.log("Sorted array: " + sortedArray);
+
+// find vowels and consonants.
+
+function vowelsAndConsonants(s) {
+  const vowels = 'aeiouAEIOU';
+    for (let i = 0; i < s.length; i++) { 
+        if(vowels.indexOf(s[i]) !== -1){
+          console.log(s[i])
+        }
+    }
+    for(let j = 0; j <s.length; j++){
+      if(vowels.indexOf(s[j]) === -1){
+        console.log(s[j])
+      }
+    }
+}
+
+vowelsAndConsonants('javascript')
